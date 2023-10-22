3d1f82493c30dcfdaea887cffe6a6d433571c225
marianne <tsayenn@gmail.com>
2023-10-21 23:13:36 -0700

    i'm like p sure queen works
    uncleaned ver

--- interpreterv1.py
+++ interpreterv1.py
@@ -1,5 +1,12 @@
 from brewparse import parse_program
 from intbase import InterpreterBase
+from enum import Enum
+
+class ErrorType(Enum):
+    TYPE_ERROR = 1
+    NAME_ERROR = 2  # if a variable or function name can't be found
+    FAULT_ERROR = 3  # used if an object reference is null and used to make a call
+    # Add others here
 
 class Interpreter(InterpreterBase):
     def __init__(self, console_output = True, inp = None, trace_output = False):
@@ -9,23 +16,20 @@ class Interpreter(InterpreterBase):
     def run(self, program):
         ast = parse_program(program)
         main_node = ast.dict['functions'][0]
-        print(main_node)
         if (main_node.dict['name'] != 'main'):
-            super().error(2, 'No main() function was found')
-        else: self.run_func(main_node)
-        print(self.int_dict)
-
+            super().error(ErrorType.NAME_ERROR, 'No main() function was found')
+        else: self.run_func(main_node) # run main func if it exists
+        
     def run_func(self, func_node):
-        # create a function that loops through all statements
+        # loops through all statements and runs each
         statement_list = func_node.dict['statements']
         for statement in statement_list:
             self.run_statement(statement)
-        #return statement_list
 
     def run_statement(self, statement_node):
         if (self.is_assignment(statement_node)):
             self.do_assignment(statement_node)
-        else:
+        elif (self.is_func_call(statement_node)):
             self.do_func_call(statement_node)
 
     def is_assignment(self, statement_node):
@@ -35,25 +39,23 @@ class Interpreter(InterpreterBase):
 
     def do_assignment(self, statement_node):
         var = statement_node.dict['name']
-        # print(var)
         expression = statement_node.dict['expression']
         expression_result = self.eval_expression(expression)
-        self.int_dict[var] = expression_result
-        # put function here on handling expression
-        # assign result of expression to var
+        self.int_dict[var] = expression_result # assigns value to var
     
-    def eval_expression(self, expression_node):
-        # print(expression_node.elem_type)
+    def eval_expression(self, expression_node): # returns value of expression
         if (self.is_value(expression_node) or self.is_var(expression_node)):
-            return self.get_value(expression_node) # returns value
+            return self.get_value(expression_node)
         elif (self.is_binary_op(expression_node)):
             return self.eval_binary_op(expression_node)
+        elif (self.is_func_call(expression_node)):
+            return self.do_func_call(expression_node)
     
     def is_value(self, expression_node):
         if (expression_node.elem_type == 'int' or expression_node.elem_type == 'string'):
             return True
         
-    def get_value(self, node):
+    def get_value(self, node): # returns value of val or var
         if (self.is_value(node)):
             return(node.dict['val'])
         elif (self.is_var(node)):
@@ -61,13 +63,7 @@ class Interpreter(InterpreterBase):
             if (var in self.int_dict):
                 return self.int_dict.get(var)
             else:
-                super().error(2, 'Variable ' + var + ' not found')
-    
-    def get_value_of_var(self, variable):
-        if (variable in self.int_dict):
-            return self.int_dict.get(variable)
-        else:
-            super().error(2, 'Variable ' + variable + ' not found')
+                super().error(ErrorType.NAME_ERROR, 'Variable ' + var + ' not found')
         
     def is_var(self, expression_node):
         if (expression_node.elem_type == 'var'):
@@ -82,28 +78,6 @@ class Interpreter(InterpreterBase):
         op1 = expression_node.dict["op1"]
         op2 = expression_node.dict["op2"]
 
-        # print('op1: ' + str(op1))
-        op1_val = self.get_value(op1)
-
-        # if (self.is_value(op1) or self.is_var(op1)):
-        #     op1_val = self.get_value(op1)
-        #     if (self.is_value(op2) or self.is_var(op2)):
-        #         op2_val = self.get_value(op2)
-        #         print('operation reveal! : ' + str(self.do_operation(operation, op1_val, op2_val)))
-        #         return self.do_operation(operation, op1_val, op2_val)
-        #     elif (self.is_binary_op(op2)):
-        #         op2_val = self.eval_binary_op(op2)
-        #         return self.do_operation(operation, op1_val, op2_val)
-        # elif (self.is_binary_op(op1)): 
-        #     op1_val = self.eval_binary_op(op1)
-        #     if (self.is_value(op2) or self.is_var(op2)):
-        #         op2_val = self.get_value(op2)
-        #         print('operation reveal! : ' + str(self.do_operation(operation, op1_val, op2_val)))
-        #         return self.do_operation(operation, op1_val, op2_val)
-        #     elif (self.is_binary_op(op2)):
-        #         op2_val = self.eval_binary_op(op2)
-        #         return self.do_operation(operation, op1_val, op2_val)
-        
         if (self.is_value(op1) or self.is_var(op1)):
             op1_val = self.get_value(op1)
         elif (self.is_binary_op(op1)):
@@ -111,31 +85,56 @@ class Interpreter(InterpreterBase):
             
         if (self.is_value(op2) or self.is_var(op2)):
                 op2_val = self.get_value(op2)
-                print('operation reveal! : ' + str(self.do_operation(operation, op1_val, op2_val)))
                 return self.do_operation(operation, op1_val, op2_val)
         elif (self.is_binary_op(op2)):
                 op2_val = self.eval_binary_op(op2)
                 return self.do_operation(operation, op1_val, op2_val)
-
-        # print(op1_val)
-        # print('op2: ' + str(op2))
     
     def do_operation(self, operation, op1, op2):
+        if (type(op1) != int or type(op2) != int):
+            return super().error(ErrorType.TYPE_ERROR, 'Incompatible types for arithmetic operation')
         if (operation == '+'):
             return op1 + op2
         elif (operation == '-'):
             return op1 - op2
-
+        
+    def is_func_call(self, statement_node):
+        if (statement_node.elem_type == 'fcall'):
+            return True
+        else: return False
+    
     def do_func_call(self, statement_node):
         func = statement_node.dict['name']
-
+        args = statement_node.dict['args']
+        if (func == 'print'):
+            output = ''
+            for arg in args:
+                if (self.is_value(arg)):
+                    output = output + self.get_value(arg)
+                elif (self.is_var(arg)):
+                    output = output + str(self.get_value(arg))
+                elif (self.is_binary_op(arg)):
+                    output = output + str(self.eval_binary_op(arg))
+                else:
+                    return super().error(ErrorType.TYPE_ERROR, 'Invalid argument passed in')
+            super().output(output)
+        elif (func == 'inputi'):
+            if (len(args) > 1):
+                return super().error(ErrorType.NAME_ERROR, 'No inputi() function found that takes > 1 parameter')
+            else:
+                for arg in args:
+                    if (self.is_value(arg)):
+                        super().output(self.get_value(arg))
+                    user_input = super().get_input()
+                    return int(user_input)
+                        
 
 def main():
     #all programs will be provided to your interpreter as a python string,
     # just as shown here.
     program_source = """func main() {
         x = 5 + 6;
-        print("The sum is: ", x);
+        print("The sum is: ", x, " not ", 5 + 5);
     }
     """
 
@@ -144,19 +143,30 @@ def main():
         y = 6;
         z = "tester";
         x = z;
+        print(x,y,z);
+        print(x, " ", y, " ", z);
     }
     """
 
     program_source3 = """func main() {
         y = 10;
         x  = 1 + (10 + (9 - y));
+        print(x);
+        print(y);
+    }
+    """
+
+    program_source4 = """func main() {
+        foo = 5;
+        print("The answer is: ", (10 + foo) - 6, "!");
+        inputi("Enter a value just cuz!");
     }
     """
     
     # this is how you use our parser to parse a valid Brewin program into 
     # an AST:
 
-    i.run(program_source3)
+    i.run(program_source4)
     #print(parse_program(program_source3))
 
     '''
